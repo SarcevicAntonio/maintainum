81233cc235e2fe0c6fef8af1cd65f3475a0a2338
Antonio Sarcevic <toni@linkplay.space>
2026-05-17 19:51:38 +0200

    add login test

--- src/routes/(auth)/login/page.svelte.e2e.ts
+++ src/routes/(auth)/login/page.svelte.e2e.ts
@@ -1,18 +1,15 @@
 import { expect, test } from '$lib/e2e/test'
 
-test.describe('/register', () => {
-	test('can register', async ({ page, user }) => {
+test.describe('/login', () => {
+	test('can login', async ({ page, user }) => {
+		await user.create()
 		await page.goto('/')
 		await page.waitForURL('**/login')
-		await page.getByRole('link', { name: 'register' }).click()
 		await page.getByRole('textbox', { name: 'email' }).fill(user.email)
 		await page
 			.getByRole('textbox', { name: 'password', exact: true })
 			.fill(user.password)
-		await page
-			.getByRole('textbox', { name: 'confirm password' })
-			.fill(user.password)
-		await page.getByRole('button', { name: 'register' }).click()
+		await page.getByRole('button', { name: 'login' }).click()
 		await page.waitForURL('**/')
 		expect(page.getByRole('heading', { name: 'maintainum' })).toBeVisible()
 	})
