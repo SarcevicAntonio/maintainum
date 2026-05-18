746b3829fc25fa03f6bdc926e4dec28a7e42b5d0
Antonio Sarcevic <toni@linkplay.space>
2026-05-18 18:10:07 +0200

    join join

---
+++ src/routes/(app)/join_list/[list]/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { resolve } from '$app/paths'
+
+	const { data } = $props()
+</script>
+
+<p>you have been invited to join a task list on maintainum.</p>
+{#if !data.user}
+	<p>to join the list, you need to first authenticate.</p>
+
+	TODO: add back query params and redirect in load functions
+	<a href={resolve('/login')}>login</a>
+	<a href={resolve('/register')}>register</a>
+{:else}
+	TODO: add join handler
+	<form method="POST">
+		<button type="submit">join list</button>
+	</form>
+{/if}
